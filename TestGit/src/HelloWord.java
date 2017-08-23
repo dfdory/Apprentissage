cc46b5db8c95a28279f6fe9b1a8e30b8a9317244
dfdory <doriane.fotsing-djoukoua.1@ens.etsmtl.ca>
2017-08-23 19:32:15 -0400

    commentaire deux. creation

--- TestGit/src/HelloWord.java
+++ TestGit/src/HelloWord.java
@@ -4,6 +4,7 @@ public class HelloWord {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bonjour dodo tu vas y trouver . vasy");
+		System.out.println("Ajout de la ligne dododo");
 
 	}
 
