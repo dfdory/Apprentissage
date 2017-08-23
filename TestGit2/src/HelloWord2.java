937450f82dcfba493695e5a233a279a87e7c4a0f
dfdory <doriane.fotsing-djoukoua.1@ens.etsmtl.ca>
2017-08-23 19:28:58 -0400

    envoi projet 2 pour confirmation

---
+++ TestGit2/src/HelloWord2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author dory
+ *
+ */
+public class HelloWord2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("deuxieme essai pour etre sur du processus");
+	}
+
+}
