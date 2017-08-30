aea2324998d9174220def461ea0c8060188c5238
dfdory <doriane.fotsing-djoukoua.1@ens.etsmtl.ca>
2017-08-30 09:26:27 -0400

    Test pour la lecture et l'ecriture au clavier

--- TestGit/src/HelloWord.java
+++ TestGit/src/HelloWord.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
 
 public class HelloWord {
+
+	private static Scanner sc;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bonjour dodo tu vas y trouver . vasy");
-		System.out.println("Ajout de la ligne dododo");
+		/* Debut Lecture et ecriture au clavier */
+		System.out.println("entrez un nbre");
+		sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		System.out.println("entrez une chaine");
+		sc.nextLine(); // insctruction permettant de vider la chaine
+		String x = sc.nextLine();
+
+		System.out.println("FIN : " + i + " : " + x);
+		char carac = x.charAt(0);
+		System.out.println("le premier caractere  de la chaine est : " + carac);
+
+		/* Fin Lecture et ecriture au clavier */
 
 	}
 
